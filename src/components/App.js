4dec7361f2a64e524f29f708daaf8eb82ae5828c
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-03-20 22:38:42 +0900

    Introduce axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    onSearchSubmit = (term) => {
-        console.log(term);
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID 6QYzNjZMGEQDkRWz2YihcEhzQaqGkAoCjTnkvGL5OGM'
+            }
+        });
     };
+
     render() {
         return (
             <div className="ui container">
