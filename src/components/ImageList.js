2861a792c287ae6793c3910312d31c1278e82a5c
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-03-20 23:14:39 +0900

    Show images as search result

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,10 +2,10 @@ import React from "react";
 
 
 const ImageList = (props) => {
-    console.log(props.images);
-    return (
-        <div>Image List</div>
-    )
+    const images = props.images.map(image => {
+        return <img key={image.id} src={image.urls.regular} alt=""/>
+    });
+    return <div>{images}</div>
 };
 
 export default ImageList;
